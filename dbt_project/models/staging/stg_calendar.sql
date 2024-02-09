5c102930f057cbf0e7e4a97bfe7587dd8f3e4af7
Teddy <teddy@wennecorp.com>
2024-02-09 14:25:36 -0500

    actually mostly answering question 1

--- dbt_project/models/staging/stg_calendar.sql
+++ dbt_project/models/staging/stg_calendar.sql
@@ -1,5 +1,8 @@
 SELECT
-  * EXCEPT(reservation_id)
+  * EXCEPT(available, reservation_id)
+  ,CASE WHEN available = 't' THEN TRUE
+  WHEN available = 'f' THEN FALSE
+  END AS available
   ,SAFE_CAST(reservation_id AS INT64) AS reservation_id
 FROM
   `hopeful-theorem-413815.source_hubspot_ae_tech_assessment.calendar`
---
+++ queries/01_amenity_review.sql
@@ -0,0 +1,32 @@
+/*
+
+#1 - Amenity Revenue
+Write a query to find the total revenue and percentage of revenue by month segmented by whether or not air conditioning exists on the listing.
+Tip: For example, only 21.2% of revenue in July 2022 came from listings without air conditioning.
+
+*/
+
+SELECT
+  month
+  ,total_revenue
+  ,revenue_with_air_conditioning / total_revenue AS percent_revenue_air_conditioning
+  ,1 - (revenue_with_air_conditioning / total_revenue) AS percent_revenue_no_air_conditioning
+FROM
+  (
+  SELECT
+    FORMAT_DATE('%Y-%m', date) AS month
+    ,SUM(price) AS total_revenue
+    ,SUM(
+      CASE WHEN 'Air conditioning' IN UNNEST(amenities)
+      THEN price
+      END
+    ) AS revenue_with_air_conditioning
+  FROM
+    `hopeful-theorem-413815.dbt_hubspot_ae_tech_assessment.mart`
+  WHERE
+    NOT available
+  GROUP BY
+    month
+  )
+ORDER BY
+  month ASC
