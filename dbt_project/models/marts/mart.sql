7b2127b8ea8790cf29055a178940c0ab085019b5
Teddy <teddy@wennecorp.com>
2024-02-11 10:06:54 -0500

    answering question 2

--- dbt_project/models/marts/mart.sql
+++ dbt_project/models/marts/mart.sql
@@ -1,4 +1,9 @@
 SELECT
-  *
+  c.*
+  ,l.neighborhood
 FROM
-  {{ ref('int_calendar_with_amenity') }}
+  {{ ref('int_calendar_with_amenity') }} AS c
+LEFT JOIN
+  {{ ref('stg_listings') }} AS l
+ON
+  c.listing_id = l.id
---
+++ queries/02_neighborhood_pricing.sql
@@ -0,0 +1,36 @@
+/*
+
+#2 - Neighborhood Pricing
+
+Write a query to find the average price increase for each neighborhood from July 12th 2021 to July 11th 2022.
+
+Tip:
+  For example, the Back Bay neighborhood only has one listing, so the difference of $44
+  is the average for the whole neighborhood based solely on listing 10813.
+
+*/
+
+SELECT
+  date
+  ,neighborhood
+  ,COUNT(*) AS n_listings
+  ,AVG(price - price_364_days_prior) AS avg_price_increase_vs_364_days_prior
+FROM
+  (
+  SELECT
+    date
+    ,neighborhood
+    ,listing_id
+    ,price
+    ,ANY_VALUE(price) OVER(PARTITION BY neighborhood, listing_id ORDER BY UNIX_DATE(date) ASC RANGE BETWEEN 364 PRECEDING AND 364 PRECEDING) AS price_364_days_prior
+  FROM
+    `hopeful-theorem-413815.dbt_hubspot_ae_tech_assessment.mart`
+  )
+WHERE
+  date = '2022-07-11'
+GROUP BY
+  date
+  ,neighborhood
+ORDER BY
+  date
+  ,neighborhood
